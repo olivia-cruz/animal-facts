14bf37b564858326fac24216a3a768f239d78f95
Olivia-cruz <117705365+Olivia-cruz@users.noreply.github.com>
2024-05-19 23:21:11 -0500

    adding animal facts and route to generate fact

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+import json
+import random
+
 from flask import Flask
 
-app = Flask(__name__)
+animal_facts = []
+
+def create_app():
+    animal_file = open("animal_facts.json")
+    data = json.load(animal_file)
+    for animal, fact in data.items():
+        animal_facts.append((animal, fact))
+    return Flask(__name__)
+
+app = create_app()
 
 @app.route("/")
 def home():
     return "basic flask app"
+
+@app.route("/facts")
+def generate_fact():
+    animal_fact = animal_facts[random.randint(0, 19)]
+    return f"{animal_fact[0]}: {animal_fact[1]}"
 
 if __name__ == "__main__":
     app.run(debug=True)
