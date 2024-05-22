ef13c8816110c7a9b52a1218e39d2b978e2bec3f
Olivia-cruz <117705365+Olivia-cruz@users.noreply.github.com>
2024-05-22 00:28:20 -0500

    Updated Project test file.

--- test_microservice.py
+++ test_microservice.py
@@ -8,15 +8,5 @@ def get_random_fact():
         animal, fact = list(data.items())[0]
         print(f"Random Animal Fact: {animal} - {fact}")
         
-# def get_random_fact():
-#     url = '' 
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         data = response.json()
-#         animal, fact = list(data.items())[0]
-#         print(f"Random Animal Fact: {animal} : {fact}")
-#     else:
-#         print(f"Failed to get random fact. Status code: {response.status_code}")
-
 if __name__ == "__main__":
     get_random_fact()
